e202c3411397df7bae2fe28ad30ae77acb9a5f21
Yahiyamuhammed <yahiyamuhammed98@gmail.com>
2025-02-23 22:42:44 +0530

    studied fetch in react js

--- vite-project/src/components/useRef.jsx
+++ vite-project/src/components/useRef.jsx
@@ -1,10 +1,25 @@
-import { useRef, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 
 export default function(){
-    const [number,setNumber]=useState()
+    const [number,setNumber]=useState(0)
+    const [fact,setFact]=useState('')
     const numRef=useRef()
-    function getFact() {
+    
+    useEffect(()=>{
+        async function setFacts (){
+            const result= await fetch(`http://numbersapi.com/${number}`)
+            const texts=await result.text()
+            setFact(texts)
+            console.log(texts);
+        }
+        setFacts()
+    },[number])
+    
+   
+    async function getFact() {
         setNumber(numRef.current.value)
+        
+        
     }
     return (
         <>
@@ -12,6 +27,7 @@ export default function(){
                 <input ref={numRef} type="text" />
                 <button onClick={getFact}>get fact</button>
                 <p>{number}</p>
+                <h1>{fact}</h1>
             </div>
         </>
     )
